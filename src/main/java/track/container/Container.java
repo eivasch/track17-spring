87450995fe15ab9ecb9f9a8a3c739842e8e3ba65
elena <elenaiv100@gmail.com>
2017-04-10 19:34:29 +0300

    read config

--- src/main/java/track/container/Container.java
+++ src/main/java/track/container/Container.java
@@ -1,26 +1,72 @@
 package track.container;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import track.container.config.Bean;
+import track.container.config.Property;
+import track.container.config.ValueType;
 
 /**
  * Основной класс контейнера
  * У него определено 2 публичных метода, можете дописывать свои методы и конструкторы
  */
 public class Container {
-
+    private List<Bean> beans;
+    private Map<String, Object> objByName;
+    private Map<String, Object> objByClassName;
 
     // Реализуйте этот конструктор, используется в тестах!
     public Container(List<Bean> beans) {
+        objByName = new HashMap<>();
+        objByClassName = new HashMap<>();
+        this.beans = beans;
+    }
 
+    private String getSetMethodName(String name) {
+        return "set" + name.substring(0, 1).toUpperCase() + name.substring(1);
+    }
+
+    private Object createObject(Bean bean) throws Exception {
+        Class clazz = Class.forName(bean.getClassName());
+        Object newObject = clazz.newInstance();
+        Map<String, Property> properties = bean.getProperties();
+        for (Property property: properties.values()) {
+            Field field = clazz.getDeclaredField(property.getName());
+            field.setAccessible(true);
+            if (property.getType().equals(ValueType.VAL)) {
+                field.set(newObject, Integer.parseInt(property.getValue()));
+            } else {
+                Object refObject = getById(property.getValue());
+                String methodName = getSetMethodName(property.getName());
+                Method method = clazz.getDeclaredMethod(methodName, field.getType());
+                method.invoke(newObject, refObject);
+                field.set(newObject, refObject);
+            }
+        }
+        objByClassName.put(bean.getClassName(), newObject);
+        objByName.put(bean.getId(), newObject);
+        return newObject;
     }
 
     /**
      *  Вернуть объект по имени бина из конфига
      *  Например, Car car = (Car) container.getById("carBean")
      */
-    public Object getById(String id) {
+    public Object getById(String id) throws Exception {
+        if (objByName.containsKey(id)) {
+            return objByName.get(id);
+        } else {
+            for (Bean bean : beans) {
+                if (bean.getId().equals(id)) {
+                    return createObject(bean);
+                }
+            }
+        }
         return null;
     }
 
@@ -28,7 +74,16 @@ public class Container {
      * Вернуть объект по имени класса
      * Например, Car car = (Car) container.getByClass("track.container.beans.Car")
      */
-    public Object getByClass(String className) {
+    public Object getByClass(String className) throws Exception {
+        if (objByClassName.containsKey(className)) {
+            return objByClassName.get(className);
+        } else {
+            for (Bean bean : beans) {
+                if (bean.getClassName().equals(className)) {
+                    return createObject(bean);
+                }
+            }
+        }
         return null;
     }
 }
--- src/main/java/track/container/Main.java
+++ src/main/java/track/container/Main.java
@@ -1,11 +1,19 @@
 package track.container;
 
+import track.container.beans.Car;
+import track.container.config.Bean;
+import track.container.config.ConfigReader;
+import track.container.config.InvalidConfigurationException;
+
+import java.io.File;
+import java.util.List;
+
 /**
  *
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         /*
 
@@ -14,13 +22,13 @@ public class Main {
          */
 
 //        // При чтении нужно обработать исключение
-//        ConfigReader reader = new JsonReader();
-//        List<Bean> beans = reader.parseBeans("config.json");
-//        Container container = new Container(beans);
-//
-//        Car car = (Car) container.getByClass("track.container.beans.Car");
-//        car = (Car) container.getById("carBean");
-
-
+        JsonConfigReader reader = new JsonConfigReader();
+        File file = new File("/home/pufelka/Tehnotrack/java/src/main/resources/config.json");
+        List<Bean> beans;
+        beans = reader.parseBeans(file);
+        Container container = new Container(beans);
+        Car car = (Car) container.getByClass("track.container.beans.Car");
+        car = (Car) container.getById("carBean");
+        System.out.println(car.toString());
     }
 }
